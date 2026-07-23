8187f73b63f3aa4f948ffc1ae2398333fd6b4fe7
jkcreating <jamilekarinee@gmail.com>
2026-07-22 23:24:48 -0300

    Add files via upload

--- JS/TÓPICOS/script.js
+++ JS/TÓPICOS/script.js
@@ -2,18 +2,26 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(event){
     event.preventDefault(); 
 
-    const weight = document.getElementById('weight').value; 
-    const height = document.getElementById('height').value; 
+    /*const weight = document.getElementById('weight').value; 
+    const height = document.getElementById('height').value; */
+    
+    const nota_atividade = document.getElementById('nota_atividade').value; 
+    const nota_prova = document.getElementById('nota_prova').value; 
 
+    /*Média Parcial: [(4xN1)+(6xN2)]/10*/
+    /*const bmi = (weight / (height * height)).toFixed(2); 
+    const value = document.getElementById('value'); 
+    let description = ''; 
+    value.classList.add('attention')*/
 
-    const bmi = (weight / (height * height)).toFixed(2); 
+    const nota_1_media = ((Number(nota_atividade))+(Number(nota_prova))).toFixed(2); 
     const value = document.getElementById('value'); 
     let description = ''; 
     value.classList.add('attention')
 
     document.getElementById('infos').classList.remove('hidden');
 
-    if (bmi < 4){
+    if (nota_1_media < 4){
         description = 'abaixo do peso'; 
     }
     else{
@@ -25,7 +33,7 @@ form.addEventListener('submit', function(event){
 
 
 
-    value.textContent = bmi; 
+    value.textContent = nota_1_media; 
     document.getElementById('description').textContent = description
 })
 
